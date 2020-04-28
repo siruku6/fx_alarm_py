6ce0fb4abd8a70e826ff57c5d73afc9e80e484b3
Seiya Sasaki <sirukufarios@gmail.com>
2020-04-28 21:02:36 +0900

    Unittest: trade_rulesのscalpingの単体テストを追加

--- tests/test_scalping.py
+++ tests/test_scalping.py
@@ -35,33 +35,22 @@ class TestScalping(unittest.TestCase):
     #         self.assertEqual(row['exitable'], row['correct_exitable'], index)
     #         self.assertEqual(row['exitable_price'], row['correct_exitable_price'], index)
 
-    # def test_no_exits_by_bollinger(self):
-    #     # test-data
-    #     test_df = pd.DataFrame.from_dict(
-    #         {
-    #             'long_in_the_band': [True, False, 115.5, 114.5, TestScalping.DummyPlus2sigma, TestScalping.DummyMinus2sigma, np.nan, np.nan],
-    #             'short_in_the_band': [False, True, 115.5, 114.5, TestScalping.DummyPlus2sigma, TestScalping.DummyMinus2sigma, np.nan, np.nan],
-    #             'no_in_the_band': [False, False, 115.5, 114.5, TestScalping.DummyPlus2sigma, TestScalping.DummyMinus2sigma, np.nan, np.nan],
-    #             'no_over_the_band': [False, False, 116.5, 114.5, TestScalping.DummyPlus2sigma, TestScalping.DummyMinus2sigma, np.nan, np.nan],
-    #             'no_below_the_band': [False, False, 115.5, 112.5, TestScalping.DummyPlus2sigma, TestScalping.DummyMinus2sigma, np.nan, np.nan],
-    #         },
-    #         columns=[
-    #             'is_long', 'is_short', 'high', 'low', 'plus2sigma', 'minus2sigma',
-    #             'correct_exitable', 'correct_exitable_price'
-    #         ],
-    #         orient='index'
-    #     )
-    #     exitable, exitable_price = scalping.exits_by_bollinger(
-    #         candles=test_df[['high', 'low']],
-    #         long_indexes=test_df.is_long, short_indexes=test_df.is_short,
-    #         plus2sigma=test_df.plus2sigma, minus2sigma=test_df.minus2sigma
-    #     )
-    #     test_df['exitable'] = exitable
-    #     test_df['exitable_price'] = exitable_price
-
-    #     for index, row in test_df.iterrows():
-    #         self.assertTrue(np.isnan(row['exitable']), index)
-    #         self.assertTrue(np.isnan(row['exitable_price']), index)
+    def test_is_exitable_by_stoc_cross(self):
+        test_dicts = [
+            {'direction': 'long', 'stod': 40, 'stosd': 90, 'exitable': True},
+            {'direction': 'long', 'stod': 70, 'stosd': 80, 'exitable': True},
+            {'direction': 'long', 'stod': 80, 'stosd': 70, 'exitable': False},
+            {'direction': 'short', 'stod': 90, 'stosd': 40, 'exitable': True},
+            {'direction': 'short', 'stod': 80, 'stosd': 70, 'exitable': True},
+            {'direction': 'short', 'stod': 70, 'stosd': 80, 'exitable': False}
+        ]
+        for row in test_dicts:
+            is_exitable = scalping.is_exitable_by_stoc_cross(
+                direction=row['direction'],
+                stod=row['stod'],
+                stosd=row['stosd']
+            )
+            self.assertEqual(is_exitable, row['exitable'])
 
     def test_is_exitable_by_bollinger(self):
         test_dicts = [
