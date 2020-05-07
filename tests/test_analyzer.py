612d04429d56c85ee8495f68d7bf3a44c36299af
Seiya Sasaki <sirukufarios@gmail.com>
2020-05-07 23:08:19 +0900

    Unittest: テストのエラーを修正

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -7,16 +7,14 @@ from tests.fixtures.d1_stoc_dummy import d1_stoc_dummy
 
 @pytest.fixture(scope='module', autouse=True)
 def analyzer_client():
-    analyzer_client = Analyzer()
-
-    print('created!')
-    yield(analyzer_client)
-    print('closed!')
+    yield Analyzer()
 
 
-def test_1(analyzer_client):
+def test_get_d1_stoc(analyzer_client):
     d1_stoc_df = pd.DataFrame.from_dict(d1_stoc_dummy)
-    candles = d1_stoc_df[['open', 'high', 'low', 'close']]
+    candles = d1_stoc_df[['open', 'high', 'low', 'close']].copy()
+    candles.loc[:, 'time'] = pd.date_range(end='2020-05-07', periods=100)
+    candles.set_index('time', inplace=True)
     analyzer_client.calc_indicators(candles, d1_candles=candles, stoc_only=True)
     result = analyzer_client.get_d1_stoc()
 
