74df161fba63ea3104fc8c8a9a348c9a38b19a22
Seiya Sasaki <sirukufarios@gmail.com>
2020-05-09 18:05:01 +0900

    Unittest: add the two tests for client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,12 @@
 # Open modules
 import unittest
-from datetime import datetime
+import datetime
 from unittest.mock import patch, MagicMock
 
 # My-made modules
 import models.oanda_py_client as watcher
 from tests.oanda_dummy_responses import *
+
 
 class TestClient(unittest.TestCase):
     #  - - - - - - - - - - - - - -
@@ -73,9 +74,32 @@ class TestClient(unittest.TestCase):
                 result = self.__watcher_instance.request_open_trades()
         self.assertIsInstance(int(self.__watcher_instance._OandaPyClient__last_transaction_id), int)
 
-    def test_request_market_ordering(self):
+    def test_failing_market_ordering(self):
         result = self.__watcher_instance.request_market_ordering(stoploss_price=None)
         self.assertTrue('error' in result)
+
+    def test_market_ordering(self):
+        stoploss_price = 111.111
+        dummy_response = {
+            'orderCreateTransaction': {
+                'instrument': 'dummy_instrument',
+                'units': '1000',
+                'time': '2020-01-13T12:34:56.912794035Z',  # 2019-04-18T14:05:59.912794035Z
+                'stopLossOnFill': str(stoploss_price)
+            }
+        }
+        with patch('oandapyV20.API.request', return_value=dummy_response):
+            response = self.__watcher_instance.request_market_ordering('', stoploss_price)
+            self.assertEqual(response, dummy_response['orderCreateTransaction'])
+
+        with patch('oandapyV20.API.request', return_value=dummy_response):
+            response = self.__watcher_instance.request_market_ordering('-', stoploss_price)
+            self.assertEqual(response, dummy_response['orderCreateTransaction'])
+
+        error_response = {'orderCreateTransaction': {}}
+        with patch('oandapyV20.API.request', return_value=error_response):
+            response = self.__watcher_instance.request_market_ordering('', stoploss_price)
+            self.assertEqual(response, error_response['orderCreateTransaction'], 'response が空でも動作すること')
 
     def test_request_trailing_stoploss(self):
         result = self.__watcher_instance.request_trailing_stoploss()
@@ -131,6 +155,25 @@ class TestClient(unittest.TestCase):
                 granularity=key
             )
             self.assertEqual(cnt, val, '[Client __calc_requestable_max_days] {}'.format(key))
+
+    def test___calc_requestable_time_duration(self):
+        max_count = watcher.OandaPyClient.REQUESTABLE_COUNT
+        granularties = ('M1', 'M5', 'M10', 'M15', 'M30', 'H1', 'H4', 'D')
+        durations = [
+            datetime.timedelta(minutes=time_int * max_count - 1) for time_int in [1, 5, 10, 15, 30]
+        ] + [
+            datetime.timedelta(minutes=(time_int * max_count - 1) * 60) for time_int in [1, 4]
+        ]
+        durations.append(datetime.timedelta(minutes=1 * max_count * 60 * 24))
+
+        for granularity, duration in zip(granularties, durations):
+            requestable_time_duration = self.__watcher_instance._OandaPyClient__calc_requestable_time_duration(
+                granularity=granularity
+            )
+            # print(granularity, duration)
+            self.assertEqual(
+                requestable_time_duration, duration, '[Client __calc_requestable_time_duration] {}'.format(granularity)
+            )
 
 
 if __name__ == '__main__':
