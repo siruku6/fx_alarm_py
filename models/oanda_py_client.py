8db37fff3b9052b5671786f80d4fd1e97a90f5b2
Seiya Sasaki <sirukufarios@gmail.com>
2021-01-01 19:29:18 +0900

    Oanda: removed unused comments

--- models/oanda_py_client.py
+++ models/oanda_py_client.py
@@ -40,67 +40,6 @@ class OandaPyClient():
     #
     # Public
     #
-
-    # def load_or_query_candles(self, start_time, end_time, granularity):
-    #     ''' (10分足用) 取得済みであれば mongodb から candles を取得してくれる '''
-    #     candles_accessor = MongodbAccessor(db_name='candles')
-    #     stocked_first_time, stocked_last_time = candles_accessor.edge_datetimes_of(currency_pare=self.__instrument)
-
-    #     print('[MongoDB] slide用10分足の不足分を解析・request中....')
-    #     if start_time < stocked_first_time:
-    #         candles_supplement = self.load_candles_by_duration(
-    #             start=start_time, end=stocked_first_time - datetime.timedelta(minutes=10),
-    #             granularity=granularity
-    #         )['candles'].rename(columns={'time': '_id'})
-    #         candles_supplement['_id'] = pd.to_datetime(candles_supplement._id)
-    #         candles_dict = candles_supplement.to_dict('records')
-    #         candles_accessor.bulk_insert(currency_pare=self.__instrument, dict_array=candles_dict)
-
-    #     if stocked_last_time < end_time:
-    #         candles_supplement = self.load_candles_by_duration(
-    #             start=stocked_last_time + datetime.timedelta(minutes=10), end=end_time,
-    #             granularity=granularity
-    #         )['candles'].rename(columns={'time': '_id'})
-    #         candles_supplement['_id'] = pd.to_datetime(candles_supplement._id)
-    #         candles_dict = candles_supplement.to_dict('records')
-    #         candles_accessor.bulk_insert(currency_pare=self.__instrument, dict_array=candles_dict)
-
-    #     print('[MongoDB] querying m10_candles ...')
-    #     stocked_candles = candles_accessor.query_candles(
-    #         currency_pare=self.__instrument,
-    #         start_dt=start_time, end_dt=end_time
-    #     )
-    #     del candles_accessor
-    #     print('[MongoDB] m10_candles are loaded !')
-
-    #     return stocked_candles
-
-    # def load_candles_by_duration(self, start, end, granularity='M5'):
-    #     ''' 広範囲期間チャート取得用の複数回リクエスト '''
-    #     candles = None
-    #     requestable_duration = self.__calc_requestable_time_duration(granularity)
-    #     next_starttime = start
-    #     # INFO: start から end まで1回のリクエストで取得できる場合は、取れるだけたくさん取得してしまう
-    #     next_endtime = start + requestable_duration
-
-    #     while next_starttime < end:
-    #         now = datetime.datetime.utcnow() - datetime.timedelta(minutes=1)
-    #         if now < next_endtime: next_endtime = now
-    #         response = self.query_instruments(
-    #             start=converter.to_oanda_format(next_starttime),
-    #             end=converter.to_oanda_format(next_endtime),
-    #             granularity=granularity
-    #         )
-    #         tmp_candles = prepro.to_candle_df(response)
-    #         candles = self.__union_candles_distinct(candles, tmp_candles)
-    #         print('取得済み: {datetime}まで'.format(datetime=next_endtime))
-    #         time.sleep(1)
-
-    #         next_starttime += requestable_duration
-    #         next_endtime += requestable_duration
-
-    #     return {'success': '[Client] APIリクエスト成功', 'candles': candles}
-
     # INFO: request-something (excluding candles)
     def request_is_tradeable(self):
         params = {'instruments': self.__instrument}  # 'USD_JPY,EUR_USD,EUR_JPY'
@@ -236,14 +175,6 @@ class OandaPyClient():
         response = self.__api_client.request(request_obj)
         LOGGER.info('[Client] trail: %s', response)
         return response
-
-    # def request_latest_transactions(self, count=999):
-    #     to_id = int(self.last_transaction_id)
-    #     from_id = to_id - count
-    #     from_id = max(from_id, 1)
-    #     response = self.request_transactions_once(from_id, to_id)
-    #     filtered_df = prepro.filter_and_make_df(response['transactions'], self.__instrument)
-    #     return filtered_df
 
     def request_transactions_once(self, from_id, to_id):
         params = {
