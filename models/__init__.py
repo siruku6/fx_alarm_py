d678c1ef05b47e48954798aca6a0aa45011d0905
Seiya Sasaki <sirukufarios@gmail.com>
2021-01-01 20:39:22 +0900

    Mongo: moved mongodb_accessor into clients directory

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from . import analyzer, client_manager, drawer, real_trader, alpha_trader, trader, history_librarian, mongodb_accessor
+from . import analyzer, client_manager, drawer, real_trader, alpha_trader, trader, history_librarian
