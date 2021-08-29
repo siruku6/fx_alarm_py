4743adc362dec5efd10f21fc7f3a2070e198bde0
Seiya Sasaki <sirukufarios@gmail.com>
2021-08-29 18:11:51 +0900

    Fix: enabled to execute backtest with static candles on csv file

--- models/trader.py
+++ models/trader.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import typing as t
 from models.candle_storage import FXBase
 from models.client_manager import ClientManager
 from models.analyzer import Analyzer
@@ -17,7 +18,7 @@ class Trader():
     MAX_ROWS_COUNT = 200
     TIME_STRING_FMT = '%Y-%m-%d %H:%M:%S'
 
-    def __init__(self, operation='backtest', days=None):
+    def __init__(self, operation: str = 'backtest', days: t.Optional[int] = None):
         '''
         Parameters
         ----------
@@ -28,24 +29,26 @@ class Trader():
         -------
         None
         '''
-        need_request = True
+        need_request: bool = True
         if operation in ('backtest', 'forward_test'):
-            result = ClientManager.select_instrument()
-            self._instrument = result[0]
-            self._static_spread = result[1]['spread']
+            selected_inst: t.List[str, float] = ClientManager.select_instrument()
+            self._instrument: str = selected_inst[0]
+            self._static_spread: float = selected_inst[1]['spread']
             self.__set_drawing_option()
-            self._stoploss_buffer_pips = i_face.select_stoploss_digit() * 5
-            days = i_face.ask_number(msg='何日分のデータを取得する？(半角数字): ', limit=365)
-            need_request = i_face.ask_true_or_false(
+            self._stoploss_buffer_pips: float = i_face.select_stoploss_digit() * 5
+            need_request: bool = i_face.ask_true_or_false(
                 msg='[Trader] Which do you use ?  [1]: current_candles, [2]: static_candles :'
             )
+            days: int = i_face.ask_number(msg='何日分のデータを取得する？(半角数字): ', limit=365)
         self.__init_common_params(operation, days=days)
+        self.__m10_candles: t.Optional[pd.DataFrame] = None
+        result: t.Dict[str, str] = self.__prepare_candles(operation, need_request, days).get('info')
 
-        result = self.__prepare_candles(operation, need_request, days).get('info')
         if result is not None:
             print(result)
             return
 
+        self.__prepare_long_span_candles(days=days, need_request=need_request)
         self._ana.calc_indicators(FXBase.get_candles(), long_span_candles=FXBase.get_long_span_candles())
         self._indicators = self._ana.get_indicators()
         self._initialize_position_variables()
@@ -57,10 +60,12 @@ class Trader():
         )
         self._drawer = None
 
-    def __init_common_params(self, operation, days):
-        self._operation = operation
-        self._ana = Analyzer()
-        self._client = ClientManager(instrument=self.get_instrument(), test=operation in ('backtest', 'forward_test'))
+    def __init_common_params(self, operation: str, days: int):
+        self._operation: str = operation
+        self._ana: Analyzer = Analyzer()
+        self._client: ClientManager = ClientManager(
+            instrument=self.get_instrument(), test=operation in ('backtest', 'forward_test')
+        )
         self._entry_rules = {
             'days': days,
             'granularity': os.environ.get('GRANULARITY') or 'M5',
@@ -70,9 +75,9 @@ class Trader():
         self._drawer = None
         self._position = None
 
-    def __prepare_candles(self, operation, need_request=True, days=None):
+    def __prepare_candles(self, operation: str, need_request: bool = True, days: int = None) -> t.Dict[str, str]:
         if need_request is False:
-            candles = pd.read_csv('./tmp/csvs/h4_candles_for_test.csv')
+            candles = pd.read_csv('tests/fixtures/sample_candles.csv')
         elif operation in ('backtest', 'forward_test'):
             self._entry_rules['granularity'] = i_face.ask_granularity()
             candles = self._client.load_long_chart(
@@ -90,15 +95,14 @@ class Trader():
             return {'info': 'exit at once'}
 
         FXBase.set_candles(candles)
-        self.__m10_candles = None
-        self.__prepare_long_span_candles(days=days)
+        if need_request is False: return {}
 
         latest_candle = self._client.call_oanda('current_price')
         self.__update_latest_candle(latest_candle)
 
         return {}
 
-    def __update_latest_candle(self, latest_candle):
+    def __update_latest_candle(self, latest_candle) -> None:
         '''
         最新の値がgranurarity毎のpriceの上下限を抜いていたら、抜けた値で上書き
         '''
@@ -111,7 +115,7 @@ class Trader():
         print('[Client] Last_H4: {}, Current_M1: {}'.format(candle_dict, latest_candle))
         print('[Client] New_H4: {}'.format(FXBase.get_candles().iloc[-1].to_dict()))
 
-    def _initialize_position_variables(self):
+    def _initialize_position_variables(self) -> None:
         self._set_position({'type': 'none'})
         self.__hist_positions = {'long': [], 'short': []}
 
@@ -128,12 +132,12 @@ class Trader():
         self._entry_rules[rule_property] = value
 
     @property
-    def m10_candles(self):
+    def m10_candles(self) -> pd.DataFrame:
         return self.__m10_candles
 
     @m10_candles.setter
-    def m10_candles(self, arg):
-        self.__m10_candles = arg
+    def m10_candles(self, arg: pd.DataFrame) -> None:
+        self.__m10_candles: pd.DataFrame = arg
 
     #
     # public
@@ -273,6 +277,7 @@ class Trader():
         gap_of_ma = self._indicators['10EMA'] - self._indicators['20SMA']
         result = gap_of_ma.shift(1) < gap_of_ma
 
+        # OPTIMIZE: 以下の部分、行数をかなり減らせそう
         # INFO: 上昇方向に勢いづいている
         is_long_steeper = np.all(
             pd.DataFrame({'bull': df_trend.bull, 'inclination': result}),
@@ -312,10 +317,16 @@ class Trader():
                 rows_num=self.__static_options['figure_option'], instrument=self.get_instrument()
             )
 
-    def __prepare_long_span_candles(self, days):
+    def __prepare_long_span_candles(self, days: int, need_request: bool = True) -> None:
         if not isinstance(days, int):
             return
-        result = self._client.load_long_chart(days=days, granularity='D')['candles']
+
+        result: pd.DataFrame
+        if need_request is False:
+            result = pd.read_csv('tests/fixtures/sample_candles_h4.csv')
+        else:
+            result = self._client.load_long_chart(days=days, granularity='D')['candles']
+
         result['time'] = pd.to_datetime(result['time'])
         result.set_index('time', inplace=True)
         FXBase.set_long_span_candles(result)
