8fd5f2e67ef4718959706afa2350701992e6436f
siruku6 <sirukufarios@gmail.com>
2022-09-04 22:33:36 +0900

    fix #220: fix collapsed SNS message

--- src/clients/sns.py
+++ src/clients/sns.py
@@ -5,9 +5,10 @@ from typing import Any, Dict
 import boto3
 
 
-def publish(dic: Dict[str, Any]) -> None:
+def publish(dic: Dict[str, Any], subject: str = "SNS message") -> None:
     sns = boto3.client("sns", region_name=os.environ.get("AWS_DEFAULT_REGION"))
     sns.publish(
+        Subject=subject,
         TopicArn=os.environ.get("SNS_TOPIC_SEND_MAIL_ARN"),
-        Message=json.dumps(dic),
+        Message=json.dumps(dic, indent=4, sort_keys=False),
     )
